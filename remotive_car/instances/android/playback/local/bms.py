d9319e79abaea5fc469ae12475c694bf651bac0a
remotivelabs <hello@remotivelabs.com>
2026-09-17 10:00:22 +0000

    Update examples [64e8892]

--- remotive_car/instances/android/playback/local/bms.py
+++ remotive_car/instances/android/playback/local/bms.py
@@ -76,11 +76,11 @@ class BMS:
 
     async def __aenter__(self):
         await self._broker_client.connect()
-        await self.bm.start()
         self._input_timeout_s = self._read_input_timeout()
         self._last_input = time.monotonic()
         self._sna_sent = False
         self._last_values: dict[str, float] = {}
+        await self.bm.start()
         self._watchdog_task = asyncio.create_task(self._watchdog())
         return self
 
